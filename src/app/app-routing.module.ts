50b6a61cc99709dd0818c4df31b213a092f71597
Saivishnu Voleti <saivoleti@Saivishnus-MacBook-Pro.local>
2019-07-11 16:02:28 -0400

    HomePage NavBar AboutUs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageComponent } from 'src/app/components/homepage/homepage.component'
+import {YtlobbyComponent} from 'src/app/components/ytlobby/ytlobby.component'
+import {AboutusComponent} from 'src/app/components/aboutus/aboutus.component'
+const routes: Routes = [
+  { path: '', redirectTo: '/homepage' , pathMatch: 'full' },
+  { path: 'homepage', component: HomepageComponent},
+  { path: 'ytlobby', component: YtlobbyComponent },
+  { path: 'aboutus', component: AboutusComponent},
+];
 
-const routes: Routes = [];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [HomepageComponent, YtlobbyComponent, AboutusComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { YtlobbyComponent } from './components/ytlobby/ytlobby.component';
 import { NgxYoutubePlayerModule } from 'ngx-youtube-player';
 import { HttpClientModule } from '@angular/common/http';
 import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 import { ChatService } from './services/chat.service';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
     AppComponent,
-    YtlobbyComponent
+  routingComponents,
+  AboutusComponent,
+  NavbarComponent
   ],
   imports: [
     BrowserModule,
